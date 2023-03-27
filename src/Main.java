dddc913e49044c5adff6e9a3436e9f5812ff38e1
VladPozolotin <vlad.yugra.nyagan@gmail.com>
2023-03-27 14:57:13 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> cart = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите номер операции:");
+            System.out.println("1. Добавить");
+            System.out.println("2. Показать");
+            System.out.println("3. Удалить");
+            int num = Integer.parseInt(scanner.nextLine());
+            switch (num) {
+                case 1:
+                    System.out.println("Какую покупку хотите добавить?");
+                    cart.add(scanner.nextLine());
+                    System.out.println("Итого в списке покупок:" + cart.size());
+                    break;
+                case 2:
+                    System.out.println("Список покупок:");
+                    if (!cart.isEmpty()) {
+                        for (String item : cart) {
+                            System.out.println((cart.lastIndexOf(item) + 1) + ". " + item);
+                        }
+                    } else {
+                        System.out.println("Пусто");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Список покупок:");
+                    if (!cart.isEmpty()) {
+                        for (String item : cart) {
+                            System.out.println((cart.lastIndexOf(item) + 1) + ". " + item);
+                        }
+                        System.out.println("Какую хотите удалить? Введите номер или название");
+                        String delItem = scanner.nextLine();
+                        try {
+                            int delInput = (Integer.parseInt(delItem) - 1);
+                            delItem = cart.get(delInput);
+                            cart.remove(delInput);
+                        } catch (NumberFormatException exception) {
+                            cart.remove(delItem);
+                        }
+                        System.out.println("Покупка \"" + delItem + "\" удалена, список покупок:");
+                        for (String item : cart) {
+                            System.out.println((cart.lastIndexOf(item) + 1) + ". " + item);
+                        }
+                    } else {
+                        System.out.println("Пусто");
+                    }
+                    break;
+                default:
+                    System.out.println("Ошибка. Неверно введён номер операции.");
+            }
+        }
+    }
+}
